6c8f0cbf55ca9f601cf5af876de4e039d5857125
Justin Lane <justinlane99@hotmail.com>
2015-02-06 16:56:03 +1300

    Add validation to batch_lines tender

--- app/models/batch_line.rb
+++ app/models/batch_line.rb
@@ -1,4 +1,10 @@
 class BatchLine < ActiveRecord::Base
   belongs_to :person
   belongs_to :fund
+  belongs_to :batch
+
+  validates :tender,
+            allow_nil: false,
+            inclusion: { in: %w(Cash Cheque Online) }
+
 end
