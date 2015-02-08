a7eca9b72aa765f0bb9865adb88655cbf5a69166
Justin Lane <justinlane99@hotmail.com>
2015-02-09 11:30:38 +1300

    -mRefactor Fund Spec

--- spec/models/fund_spec.rb
+++ spec/models/fund_spec.rb
@@ -1,66 +1,38 @@
 require_relative '../rails_helper'
 
 describe Fund do
-
   before do
     @fund = FactoryGirl.create(:fund)
   end
 
   context '#name' do
     context 'name is blank' do
-      before do
-        @fund.name = nil
-      end
-
-      it 'should be invalid' do
-        expect(@fund).to be_invalid
-      end
+      before { @fund.name = nil }
+      it { should be_invalid }
     end
 
     context 'name is too long' do
-      before do
-        @fund.name = 'a' * 101
-      end
-
-      it 'should be invalid' do
-        expect(@fund).to be_invalid
-      end
+      before { @fund.name = 'a' * 101 }
+      it { should be_invalid }
     end
-
   end
 
   context '#display_name' do
     context 'display name is blank' do
-      before do
-        @fund.display_name = nil
-      end
-
-      it 'should be invalid' do
-        expect(@fund).to be_invalid
-      end
+      before { @fund.display_name = nil }
+      it { should be_invalid }
     end
 
     context 'display name is too long' do
-      before do
-        @fund.display_name = 'a' * 11
-      end
-
-      it 'should be invalid' do
-        expect(@fund).to be_invalid
-      end
+      before { @fund.display_name = 'a' * 11 }
+      it { should be_invalid }
     end
-
   end
 
   context '#active from' do
     context 'is blank' do
-      before do
-        @fund.active_from = nil
-      end
-
-      it 'should be invalid' do
-        expect(@fund).to be_invalid
-      end
+      before { @fund.active_from = nil }
+      it { should be_invalid }
     end
 
     context ' is after active to' do
@@ -81,8 +53,7 @@ describe Fund do
     before do
       @fund.save!
     end
-
-    it 'should return a saved record' do
+    it 'should equal the first record' do
       expect(@fund).to eq(Fund.first)
     end
   end
